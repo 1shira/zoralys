f3413e4a160976d6c9ab90786abc2c44c5074b4e
Mastaofme <98327283+Mastaofme@users.noreply.github.com>
2022-12-13 13:49:23 +0100

    making data_import.js work again

--- backend/database/data_import.js
+++ backend/database/data_import.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 const pgp = pgpx();
 const db = pgp(	
-	`postgres://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.POST}/${process.env.DATABASE}`
+	`${process.env.DBTYPE}://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.PORT}/${process.env.DATABASE}`
 );
 let a = '';
 //we take the data
